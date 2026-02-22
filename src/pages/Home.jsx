1f1f2ac2f021b4fc8d3ba1da6aee432469a19495
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-23 00:03:22 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import HomeFeed from "@/pages/HomeFeed.jsx";
+
+export default function Home() {
+  return (
+    <main className="max-w-3xl mx-auto px-4 py-6">
+      <HomeFeed />
+    </main>
+  );
+}
