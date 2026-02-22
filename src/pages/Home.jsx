de4daa9e17425f2f3f89a42541c5e3e5b2f6efd8
Manish-2410 <vkdeoghar81@gmail.com>
2026-02-23 00:18:15 +0530

    Simplify Home component structure
    Refactor Home component to return HomeFeed directly.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import HomeFeed from "@/pages/HomeFeed.jsx";
+import HomeFeed from "./HomeFeed.jsx";
 
 export default function Home() {
-  return (
-    <main className="max-w-3xl mx-auto px-4 py-6">
-      <HomeFeed />
-    </main>
-  );
+  return <HomeFeed />;
 }
